d8c81c4cc49cde2a2c06983f9da4c8950a857a86
Samet <sam.kem.2000@gmail.com>
2022-06-20 23:05:49 +0300

    sss

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 const express=require('express');
+const mongoose =require('mongoose');
+const Post=require('./models/Post');
 let ejs = require('ejs');
 const app = express();
 
+mongoose.connect('mongodb://localhost/cleanblog-test-db',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
 //middleware
-app.use(express.static('public'))
 app.set("view engine","ejs");
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
-app.get('/',(req,res)=>{
+app.get('/',async (req,res)=>{
     //const blog = { id: 1, title: "Blog title", description: "Blog description" }
    // res.send(blog)
-   res.render('index');
-})
+   const posts=await Post.find({});
+   res.render('index',{
+    posts
+   });
+});
+app.get('/addpost',async(req,res)=>{
+    //res.sendFile(path.resolve(__dirname,'temp/index.html'));
+    res.render('addpost')
+  
+});
+app.post('/posts',async (req,res)=>{
+    //const blog = { id: 1, title: "Blog title", description: "Blog description" }
+   // res.send(blog)
+   await Post.create(req.body)
+   res.redirect('/')
+});
 
 const port =4000;
 app.listen(port,()=>{
