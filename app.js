8dc1f06f23cd4c02309a749617fe18c5e0bda1cd
samet kemal özdemir <samet.ozdemir@ceng.deu.edu.tr>
2022-06-20 19:16:05 +0300

    s

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express=require('express');
+let ejs = require('ejs');
 const app = express();
 
+//middleware
+app.use(express.static('public'))
+app.set("view engine","ejs");
+
 app.get('/',(req,res)=>{
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog)
+    //const blog = { id: 1, title: "Blog title", description: "Blog description" }
+   // res.send(blog)
+   res.render('index');
 })
 
 const port =4000;
